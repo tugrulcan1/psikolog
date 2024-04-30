5e4c9b0af61ac16a11c2bac0dbf6b4d0d93b6542
tugrulcan1 <tugrul-can@hotmail.com>
2024-04-30 11:16:44 +0300

    update

--- resources/views/client/home/index.blade.php
+++ resources/views/client/home/index.blade.php
@@ -16,7 +16,7 @@
 											<div class="slide_description">
 												<div class="intro-layer" data-animation="fadeInRight">
 													<h3>
-													 Sanat Terapisiasd
+													 Sanat Terapisi
 													</h3>
 												</div>
 												<div class="intro-layer" data-animation="fadeInLeft">
